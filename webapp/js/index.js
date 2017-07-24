b538e187b0301f3a083334fa3a7939ef8e520ba5
jineshmehta16 <mehtajinesh11@gmail.com>
2017-07-24 15:46:08 +0530

    - landing page changes

--- webapp/js/index.js
+++ webapp/js/index.js
@@ -1,6 +1,13 @@
 var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function($scope) {
-    $scope.firstName= "John";
-    $scope.lastName= "Doe";
+    
+    $scope.animateDown = function(type)
+    {
+        console.log('heyyyyyy');
+         $("body, html").animate({ 
+            scrollTop: $( $(this).attr('href') ).offset().top 
+        }, 600);
+    }
+
 });
